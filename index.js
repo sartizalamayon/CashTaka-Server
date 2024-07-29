8f6b8b29dddeb2e519070acc379101849007f88b
ay0-0n <sartizayo.on@gmail.com>
2024-07-29 18:57:39 +0600

    login routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
@@ -29,7 +31,45 @@ async function run() {
 
     // Models
     const database = client.db("CashTaka");
-    const UsersCollection = database.collection("users");
+    const UsersCollection = database.collection("users"); 
+
+    app.post('/users', async (req, res)=>{
+      const user = req.body
+      const salt = await bcrypt.genSalt(10);
+      const hashedPin = await bcrypt.hash(user.pin, salt);
+      user.pin = hashedPin;
+      const exists = await UsersCollection.findOne({'$or':[{email:user.email},{number: user.number}]})
+      if(!exists){
+        const response = await UsersCollection.insertOne(user) 
+      if(response.acknowledged){
+        res.send({success: true, message: "User created successfully"})
+      } 
+      else{
+        res.send({success: false, message: "Database Error: Failed to create user"})
+      }
+      }
+      else{
+        res.send({success: false, message: "Email / Phone number already exists"})
+      }
+    })
+
+    app.post('/login', async (req, res) => {
+      const data = req.body
+      existingUser = await UsersCollection.findOne({'$or':[{email:data.user},{number: data.user}]})
+      if (!existingUser) {
+        return res.status(404).json({ success: false, message: 'Invalid email or number' });
+      }
+      else{
+        const validPin = await bcrypt.compare(data.pin, existingUser.pin);
+        if(!validPin){
+          return res.status(404).json({ success: false, message: 'Invalid pin' });
+        }
+        else{
+          const token = jwt.sign(data, process.env.JWT_Secret, { expiresIn: '1h' });
+          return res.status(200).send({token, user:existingUser});
+        }
+      }
+    })
 
 
   } catch (error) {
